36d53cc0b036588cd145d2fbf5aa33f0336a3b89
claudionsc <claudio.alves20112011@hotmail.com>
2023-05-26 18:26:50 -0300

    iniciando componentes backend

---
+++ src/components/routes/back/bd-cursos/CursosBd.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CursosBd = () => {
+  return (
+    <div>CursosBd</div>
+  )
+}
---
+++ src/components/routes/back/bd-ecommerce/EcommerceBd.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const EcommerceBd = () => {
+  return (
+    <div>EcommerceBd</div>
+  )
+}
